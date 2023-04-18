af8a62f456b4088a1f53f9b63e3a3eaeba9a8ad7
maxargen07 <maxargen07@gmail.com>
2023-04-17 21:40:13 -0600

    Primeros pasos JS, tipos de variables y tipo de datos y tarea.

---
+++ 3-javascript/3.2-js-imported/js/1-console-variables.js
@@ -0,0 +1,12 @@
+// mostrar un mensaje en la consola//
+console.log("hola mundo!");
+
+/* definir e inicializar una variable para despues mostrarla en la consola */
+var fatherName = "Mateo";
+console.log(fatherName);
+
+/*definir una variable = crearla */
+var motherName;
+
+/*inicializar una variable = darle un valor */
+motherName = "Maricela";
